7a304876a672c83e04d83d71df21cb4e86f51c5d
Apoorv <apoorv@binary.com>
2017-08-01 05:27:04 +0000

    Use nginx proxy

--- bin/listener.pl
+++ bin/listener.pl
@@ -1,10 +1,16 @@
 use Data::Dumper;
 use Mojolicious::Lite;
 
-app->config(hypnotoad => {listen => ['http://*:80']});
+app->config(hypnotoad => {listen => ['http://*:3000']});
 
 sub listener {
     get '/telegram' => sub {
+        my $self = shift;
+        my $req = $self->req;
+        print Dumper($req->content) . "\n";
+        $self->render( text => "Ok");
+    };
+    get '/' => sub {
         my $self = shift;
         my $req = $self->req;
         print Dumper($req->content) . "\n";
